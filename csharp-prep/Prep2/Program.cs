29319c6ce222b6c4da989f78b1040e84b116551a
cur24001@byui.edu <cur24001@byui.edu>
2024-11-09 18:12:14 -0500

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // get user grade percentage
+        Console.Write("What is your grade percentage? ");
+        string gradePerc = Console.ReadLine();
+        int numPerc = int.Parse(gradePerc);
+
+        // determine grade letter
+        string gradeLet = " ";
+        if (numPerc >= 90) {
+            gradeLet = "A";
+        }
+        else if (numPerc >=80) {
+            gradeLet = "B";
+        }
+        else if (numPerc >=70) {
+            gradeLet = "C";
+        } 
+        else if (numPerc >=60) {
+            gradeLet = "D";
+        }
+        else {
+            gradeLet = "F";
+        }
+
+        //Determine if they passed
+        string message = "";
+        if (numPerc >= 70) {
+            message = "Congratulations! You have passed!";
+        }
+        else {
+            message = "Unfortunately, you have not passed.";
+        }
+
+        //display results
+        if (gradeLet == "A") {
+            Console.WriteLine($"{message} You got an {gradeLet}.");
+        }
+        else {
+            Console.WriteLine($"{message} You got a {gradeLet}.");
+        }
     }
 }
