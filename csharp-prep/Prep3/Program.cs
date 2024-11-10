e27aab1f154ac8b075c096e6254bd7ab7e7dcc81
cur24001@byui.edu <cur24001@byui.edu>
2024-11-09 20:40:37 -0500

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //generate random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        //ask for guess
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int numGuess = int.Parse(guess);
+
+        //tell if it's higher or lower
+        int counter = 1;
+        while (numGuess != number) {
+            if (number < numGuess) {
+                Console.WriteLine("Lower");
+            }
+            else if (numGuess < number) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("You've guessed correctly!");
+            }
+            
+            counter++;
+
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            numGuess = int.Parse(guess);  
+        }
+
+        //when guess is correct, tell how many 
+        Console.WriteLine($"You made {counter} guesses.");
     }
 }
