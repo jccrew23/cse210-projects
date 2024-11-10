8e8ece6cd992ee6e74ee2b5a31723dae5d8e892f
cur24001@byui.edu <cur24001@byui.edu>
2024-11-09 21:22:59 -0500

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //instructions
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        //make list
+        List <int> numbers = new List<int>();
+
+        int value = -1;
+
+        while (value != 0) {
+            //ask a number
+            Console.Write("Enter a number: ");
+            string stringValue = Console.ReadLine();
+            value = int.Parse(stringValue);
+
+            if (value != 0) {
+                numbers.Add(value);
+            }
+        }
+
+        //compute the sum
+        int total = 0;
+        int max = 0;
+
+        foreach (int num in numbers) {
+            total += num;
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        //calculate average
+        int length = numbers.Count;
+        int average = total / length;
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        
     }
 }
