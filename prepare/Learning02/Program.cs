c8a491f74f5a3fbad187e04344852fb92840b26e
cur24001@byui.edu <cur24001@byui.edu>
2024-11-21 22:02:45 -0500

    resume-learning project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Dell";
+        job1._jobTitle = "Developer";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+
+        Job job2 = new Job();
+
+        job2._company = "Apple";
+        job2._jobTitle = "Intern";
+        job2._startYear = 2015;
+        job2._endYear = 2019;
+
+
+        Resume resumeFull = new Resume();
+
+        resumeFull._name = "Jalie Curtis";
+
+        resumeFull._jobs.Add(job1);
+        resumeFull._jobs.Add(job2);
+
+        resumeFull.DisplayResume();
     }
+
 }
