a5f27f2f1ccbbb2d615325672e6d54b0a2065ef9
cur24001@byui.edu <cur24001@byui.edu>
2024-12-16 01:06:03 -0500

    prove 5 2

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Diagnostics;
 
+
+//Creative: Added a different animation for the Breathing Activity and added a new menu option for Kids Activities. 
 class Program
 {
     static void Main(string[] args)
     {
         Activities mainActivity = new Activities();
         Console.WriteLine($"Menu Options:");
-        Console.WriteLine($"\t1.Start breathing activity.\n\t2. Start reflecting activity\n\t3. Start listing activity\n\t4.Quit");
+        Console.WriteLine($"\t1. Start breathing activity.\n\t2. Start reflecting activity\n\t3. Start listing activity\n\t4. Start Kids Activity\n\t5. Quit");
         string userInput = Console.ReadLine();
 
-        while (userInput != "4") {
+        while (userInput != "5") {
             if (userInput == "1") {
                 BreathingActivity startBreathing = new BreathingActivity();
                 startBreathing.Run();
@@ -23,8 +25,12 @@ class Program
                 ListingActivity lists = new ListingActivity();
                 lists.Run();
             }
+            else if (userInput == "4") {
+                KidsActivites kids = new KidsActivites();
+                kids.Run();
+            }
             Console.WriteLine($"Menu Options:");
-            Console.WriteLine($"\t1.Start breathing activity.\n\t2. Start reflecting activity\n\t3. Start listing activity\n\t4.Quit");
+            Console.WriteLine($"\t1. Start breathing activity.\n\t2. Start reflecting activity\n\t3. Start listing activity\n\t4. Start Kids Activity\n\t5. Quit");
             userInput = Console.ReadLine();
         }
 
